ed6680170d71f6b125e418f7c6f9c63a7a5c0389
Curtis Upshall <curtisupshall@gmail.com>
2024-05-06 16:21:48 -0700

    Added knex to /database

---
+++ database/src/migrations/20240503223750_initial_migration.ts
@@ -0,0 +1,48 @@
+import { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+    /**
+     * Creates the software projects table
+     */
+    await knex.raw(`
+        CREATE TABLE "software_project" (
+            "software_project_id"   SERIAL PRIMARY KEY,
+            "owner_name"            VARCHAR(255) NOT NULL,
+            "project_name"          VARCHAR(255) NOT NULL,
+            "description"           VARCHAR(1024) NOT NULL,
+            "html_url"              VARCHAR(1024) NOT NULL,
+            "created_at"            TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            "updated_at"            TIMESTAMP(3) NOT NULL
+        );
+    `);
+
+    /**
+     * Creates the table used to record software project scans
+     */
+    await knex.raw(`
+        CREATE TABLE "software_project_scan" (
+            "software_project_scan_id"  SERIAL PRIMARY KEY,
+            "software_project_id"       INTEGER NOT NULL,
+            "dispatched_at"             TIMESTAMP(3) NOT NULL,
+            "completed_at"              TIMESTAMP(3) NOT NULL,
+        
+            FOREIGN KEY ("software_project_id") REFERENCES "software_project" ("software_project_id")
+        );
+    `);
+
+
+    /**
+     * Creates the table used to record software project tags discovered during scans
+     */
+    await knex.raw(`
+        CREATE TABLE "software_project_tag" (
+            "software_project_tag_id"   SERIAL PRIMARY KEY,
+            "software_project_id"       INTEGER NOT NULL,
+            "tag"                       VARCHAR(255),
+            "created_at"                TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            "updated_at"                TIMESTAMP(3) NOT NULL,
+            
+            FOREIGN KEY ("software_project_id") REFERENCES "software_project" ("software_project_id")
+        );
+    `);
+}
