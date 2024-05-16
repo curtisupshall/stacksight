d8a032e3c1973510bf2111f51efdbd84783fd1a6
Curtis Upshall <curtisupshall@gmail.com>
2024-05-16 15:59:06 -0700

    Use environment veriable for API host for upload endpoint

--- python/src/upload.py
+++ python/src/upload.py
@@ -5,15 +5,15 @@ import os
 def upload_results(project, tags):
     print('Your results are being uploaded')
     software_project_id = project.get('softwareProjectId')
+    software_project_scan_id = project.get('softwareProjectScanId')
     if not software_project_id:
         raise Exception('No software project ID was found.')
 
-    api_host = 'https://0c76-24-69-152-131.ngrok-free.app'
-    endpoint=f'/api/projects/{software_project_id}'
+    api_host = os.environ.get('PYTHON_UPLOAD_API_ENDPOINT')
+    endpoint=f'/api/projects/{software_project_id}/scans/{software_project_scan_id}'
     
     # Prepare the request
     request_url = f'{api_host}/{endpoint}'
-    
     authorization = f"Bearer {os.environ.get('STACKSIGHT_SECRET_KEY')}"
     headers = {
         'Authorization': authorization
@@ -26,10 +26,3 @@ def upload_results(project, tags):
     print('Sending now...')
     requests.patch(request_url, data=data, headers=headers)
     print('Sent.')
-
-# upload_results(
-#     {
-#         'softwareProjectId': 2,
-#     },
-#     ['javascript']
-# )
