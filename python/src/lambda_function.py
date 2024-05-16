2971c879fe03a5d6a27d0e4dbbf8e7c3d9cadc37
Curtis Upshall <curtisupshall@gmail.com>
2024-05-16 16:27:54 -0700

    Updated python script to use env var for dest folder

--- python/src/lambda_function.py
+++ python/src/lambda_function.py
@@ -2,6 +2,7 @@ from classify import classify
 from download import download_and_unzip_repo
 from upload import upload_results
 import json
+import os
 
 def lambda_handler(event, context):
     records = event.get('Records')
@@ -13,7 +14,8 @@ def lambda_handler(event, context):
         # Clone the repo
         repo_fullname = body.get('repoFullName')
         branch = body.get('branchName')
-        path_to_repo = download_and_unzip_repo(repo_fullname, branch)
+        destination_folder = os.environ.get('PYTHON_LOCAL_DESTINATION_FOLDER')
+        path_to_repo = download_and_unzip_repo(repo_fullname, branch, destination_folder)
 
         # Process the repos
         tags = classify(path_to_repo)
