14bb994deba1034c2a92327130f75af727debf8b
Curtis Upshall <curtisupshall@gmail.com>
2024-05-24 14:23:43 -0700

    Search modal text field completed

--- web/src/components/ui/header/Search.tsx
+++ web/src/components/ui/header/Search.tsx
@@ -1,11 +1,13 @@
 'use client'
 
-import { Dialog, DialogContent, Grow, Stack, TextField, Typography } from "@mui/material";
+import { Dialog, DialogContent, Grow, IconButton, InputAdornment, Stack, TextField, Typography } from "@mui/material";
 import SearchIcon from '@mui/icons-material/Search';
 import { useEffect, useState } from "react";
+import { Close } from "@mui/icons-material";
 
 export default function Search() {
     const [open, setOpen] = useState<boolean>(false);
+    const [query, setQuery] = useState<string>('');
 
     const textPlaceholder = `Search projects of technologies`
 
@@ -20,7 +22,7 @@ export default function Search() {
                         borderColor: 'rgba(0, 0, 0, 0.23)',
                         borderRadius: 16,
                         color: 'rgba(0, 0, 0, 0.65)',
-                        py: 1.5,
+                        py: 0.75,
                         pl: 2,
                         pr: 8
                     }}
@@ -33,12 +35,39 @@ export default function Search() {
                 open={open}
                 onClose={() => setOpen(false)}
                 TransitionComponent={Grow}
+                fullWidth
+                maxWidth={false}
+                PaperProps={{
+                    sx: {
+                        position: 'absolute',
+                        top: 0,
+                        mt: 0.5
+                    }
+                }}
             >
-                <DialogContent>
+                <DialogContent sx={{}}>
                     <TextField
-                        label='Search'
+                        name='stacksight__search'
+                        value={query}
+                        onChange={(event) => setQuery(event.target.value)}
+                        autoFocus
                         size='small'
                         placeholder={textPlaceholder}
+                        fullWidth
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position='start'>
+                                    <SearchIcon />
+                                </InputAdornment>
+                            ),
+                            endAdornment: query.length ? (
+                                <InputAdornment position='end'>
+                                    <IconButton onClick={() => setQuery('')}>
+                                        <Close fontSize="small"/>
+                                    </IconButton>
+                                </InputAdornment>
+                            ) : undefined
+                        }}
                     />
                 </DialogContent>
             </Dialog>
