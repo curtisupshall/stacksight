7682683b6861b2410dba32a9d78a0645625c2bac
Curtis Upshall <curtisupshall@gmail.com>
2024-05-24 14:08:23 -0700

    Created search modal (WIP)

--- web/src/components/ui/header/Search.tsx
+++ web/src/components/ui/header/Search.tsx
@@ -1,37 +1,48 @@
 'use client'
 
-import { Stack, Typography } from "@mui/material";
+import { Dialog, DialogContent, Grow, Stack, TextField, Typography } from "@mui/material";
 import SearchIcon from '@mui/icons-material/Search';
 import { useEffect, useState } from "react";
 
 export default function Search() {
     const [open, setOpen] = useState<boolean>(false);
 
-    useEffect(() => {
-        if (open) {
-            alert('opened!')
-        }
-    }, [open])
+    const textPlaceholder = `Search projects of technologies`
 
     return (
-        <a style={{ cursor: 'pointer' }} onClick={() => setOpen(true)}>
-            <Stack
-                direction='row'
-                gap={0.5}
-                sx={{
-                    border: '1px solid',
-                    borderColor: 'rgba(0, 0, 0, 0.23)',
-                    borderRadius: 16,
-                    color: 'rgba(0, 0, 0, 0.65)',
-                    py: 1.5,
-                    pl: 2,
-                    pr: 8
-                }}
+        <>
+            <a style={{ cursor: 'pointer' }} onClick={() => setOpen(true)}>
+                <Stack
+                    direction='row'
+                    gap={0.5}
+                    sx={{
+                        border: '1px solid',
+                        borderColor: 'rgba(0, 0, 0, 0.23)',
+                        borderRadius: 16,
+                        color: 'rgba(0, 0, 0, 0.65)',
+                        py: 1.5,
+                        pl: 2,
+                        pr: 8
+                    }}
+                >
+                    <SearchIcon color='inherit' />
+                    <Typography sx={{ userSelect: 'none' }}>{textPlaceholder}</Typography>
+                </Stack>
+            </a>
+            <Dialog
+                open={open}
+                onClose={() => setOpen(false)}
+                TransitionComponent={Grow}
             >
-                <SearchIcon color='inherit' />
-                <Typography sx={{ userSelect: 'none' }}>Search projects of technologies</Typography>
-            </Stack>
-        </a>
+                <DialogContent>
+                    <TextField
+                        label='Search'
+                        size='small'
+                        placeholder={textPlaceholder}
+                    />
+                </DialogContent>
+            </Dialog>
+        </>
     )
 }
 
