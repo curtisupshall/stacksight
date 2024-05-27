339fe5180205aa26521b41848eb6be83e040b910
Curtis Upshall <curtisupshall@gmail.com>
2024-05-27 14:48:18 -0700

    Created the BaseLayout component

--- web/src/app/projects/layout.tsx
+++ web/src/app/projects/layout.tsx
@@ -1,17 +1,17 @@
 import { PropsWithChildren } from "react"
-import { Breadcrumbs, Container, Link, Paper, Tabs, Typography } from "@mui/material";
+import { Breadcrumbs, Container, Link, Paper, Tab, Tabs, Typography } from "@mui/material";
+import BaseLayout from "@/components/ui/layout/BaseLayout";
 
 export default (props: PropsWithChildren) => {
     return (
         <>
-            <Container maxWidth='xl' disableGutters sx={{ px: 8 }}>
-                
+            <Container maxWidth='xl' disableGutters sx={{ px: 8, mt: -2 }}>
+                <Tabs value={0}>
+                    <Tab label='Dashboard' />
+                    <Tab label='Projects' />
+                </Tabs>
             </Container>
-            <Paper square variant="outlined" sx={{ flex: 1, pt: 4, borderLeft: 0, borderRight: 0 }}>
-                <Container maxWidth='xl' disableGutters sx={{ px: 8 }}>
-                    {props.children}
-                </Container>
-            </Paper>
+            <BaseLayout>{props.children}</BaseLayout>
         </>
     );
 }
