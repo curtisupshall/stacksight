1b5b2c8852e8f421f0ed2800a9ced582d294eb76
Curtis Upshall <curtisupshall@gmail.com>
2024-05-27 15:22:14 -0700

    Consolidated app layout cfiles

--- web/src/app/(admin)/layout.tsx
+++ web/src/app/(admin)/layout.tsx
@@ -1,14 +1,19 @@
 'use server'
 
 import type { PropsWithChildren } from 'react'
-import { Stack } from '@mui/material'
+import { Container, Stack, Tab, Tabs } from '@mui/material'
 import Header from '@/components/ui/header/Header'
+import BaseLayout from '@/components/ui/layout/BaseLayout'
+import AppTabs from '@/components/ui/header/AppTabs'
 
 export default async (props: PropsWithChildren) => {
 	return (
 		<Stack component='main' id='root' minHeight='100vh'>
 			<Header maxWidth='xl' disableGutters sx={{ px: 8 }} />
-			{props.children}
+			<Container maxWidth='xl' disableGutters sx={{ px: 8, mt: -2 }}>
+                <AppTabs />
+            </Container>
+            <BaseLayout>{props.children}</BaseLayout>
 		</Stack>
 	)
 }
