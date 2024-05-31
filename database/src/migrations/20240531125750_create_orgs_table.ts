3e932d9fdfd3b2aa1b3c32b165944dd81c3f34ea
Curtis Upshall <curtisupshall@gmail.com>
2024-05-31 13:44:22 -0700

    Amended database migration

--- database/src/migrations/20240531125750_create_orgs_table.ts
+++ database/src/migrations/20240531125750_create_orgs_table.ts
@@ -7,7 +7,9 @@ export async function up(knex: Knex): Promise<void> {
     await knex.raw(`
         CREATE TABLE "software_organization" (
             "software_organization_id"   SERIAL PRIMARY KEY,
-            "name"                       INTEGER NOT NULL,
+            "name"                       VARCHAR(64),
+            "full_name"                  VARCHAR(255),
+            "bio"                        VARCHAR(255),
             "html_url"                   VARCHAR(255),
             "avatar_url"                 VARCHAR(255),
             "created_at"                 TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
