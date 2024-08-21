2378db336042f981a7c3e47edf2b331400c25986
Curtis Upshall <curtisupshall@gmail.com>
2024-08-21 16:56:41 -0700

    STAC-37: Further deleted next-auth code.

--- web/src/components/auth/SignOutButton.tsx
+++ web/src/components/auth/SignOutButton.tsx
@@ -1,9 +1,9 @@
-import { handleSignOut } from "@/server/actions/authActions"
+// import { handleSignOut } from "@/server/actions/authActions"
 import { MenuItem } from "@mui/material"
  
 export default function SignOutButton() {
   return (
-    <form action={handleSignOut}>
+    <form>
       <MenuItem component='button' type='submit' sx={{ width: '100%' }}>Sign out</MenuItem>
     </form>
   )
--- web/src/components/auth/UserAvatar.tsx
+++ web/src/components/auth/UserAvatar.tsx
@@ -1,12 +1,7 @@
-import { auth } from "@/auth"
 import { Avatar } from "@mui/material"
  
-export default async function UserAvatar() {
-  const session = await auth()
- 
-  if (!session?.user) return null
- 
+export default async function UserAvatar() { 
   return (
-    <Avatar src={session?.user?.image ?? undefined} sx={{ ml: 1 }} />
+    <Avatar src={undefined} sx={{ ml: 1 }} />
   )
 }
