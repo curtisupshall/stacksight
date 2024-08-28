9c154809b783bae4e58dfcbff89fcdde2b5cb1b8
Curtis Upshall <curtisupshall@gmail.com>
2024-08-28 15:08:22 -0700

    STAC-38: Created todo list for libraries to add

---
+++ stack/src/libraries/frontend/todo.ts
@@ -0,0 +1,22 @@
+// vue
+// vite
+// tanstack query
+
+// Backend:
+// express
+// dotnet
+// Java Spring Boot
+
+// DevOps:
+// docker
+// k8s
+
+// CI/CD:
+// github actions
+
+// Database:
+// postgres
+// sqlite
+
+// Authentication:
+// keycloak
